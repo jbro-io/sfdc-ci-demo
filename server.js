b72f09b31dbf2f7a631d2ae3c986d8fbc77699e6
Jonathan Broquist <jonnybro@gmail.com>
2014-08-05 16:51:10 -0700

    Updates for integration with travis for CI

--- server.js
+++ server.js
@@ -2,28 +2,79 @@
 //================================================================================
 // Libraries
 //================================================================================
-var express            = require('express');
-
-//================================================================================
-// Properties
-//================================================================================
-var app  = express();
-var port = process.env.PORT || 7000;
-
-//================================================================================
-// Configuration
-//================================================================================
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-if(process.env.NODE_ENV === 'development') {
-    console.log('!! DEVELOPMENT MODE !!');
-} else if(process.env.NODE_ENV === 'production') {
-    console.log('!! PRODUCTION MODE !!');
-}
+var jsforce      = require('jsforce');
+var _            = require('underscore');
+var xmlBuilder   = require('xmlbuilder');
+var exec         = require('child_process').exec;
+var fs           = require('fs');
 
 //================================================================================
 // Initialization
 //================================================================================
-app.listen(port, function() {
-  console.log('Listening on ' + port);
+var conn = new jsforce.Connection();
+conn.login('jonathan@jbro.io', '2^Eswk7JQhxBfdbr/56qmLLkVFysbEbgClmDPpu0jf', function(err, res) {
+    if(err) return console.error(err);
+    console.log(res);
+
+    // describe org metadata
+    var xml = xmlBuilder.create('Package', {version: '1.0', encoding: 'UTF-8', standalone: true});
+    xml.root().att('xmls', 'http://soap.sforce.com/2006/04/metadata');
+
+    console.log('Retrieving salesforce metadata...');
+    conn.metadata.describe(function(err, describeResult) {
+        if(err) return console.log(err);
+
+        var metaList = [];
+        var excludedMeta = [];
+        _.each(describeResult.metadataObjects, function(metaObj) {
+            if(!metaObj.inFolder && metaObj.xmlName != 'CustomObject') {
+                metaList.push(metaObj.xmlName);
+
+                xml.ele('types')
+                    .ele('members', null, '*')
+                    .up()
+                    .ele('name', metaObj.xmlName);
+
+            }
+            else {
+                if(metaObj.xmlName !== 'CustomObject')
+                    excludedMeta.push(metaObj.xmlName);
+            }
+        });
+        console.log(excludedMeta);
+
+        console.log('Listing metadata file properties...');
+        conn.metadata.list({type:'CustomObject'}, function(err, fileProps) {
+            if(err) return console.error(err);
+
+            var customObjectElement = xml.ele('types');
+
+            _.each(fileProps, function(obj) {
+
+                customObjectElement.ele('members', null, obj.fullName);
+
+            });
+
+            customObjectElement
+                .ele('members', null, '*')
+                .up()
+                .ele('name', null, 'CustomObject');
+
+            xml.ele('version', null, '31.0');
+            console.log(xml.toString({pretty: true}));
+            // res.send(xml.end());
+
+            fs.writeFile('./package.xml', xml.end({pretty: true, indent:'    ', newline:'\n'}), function(err) {
+                if(err) return console.log(err);
+
+                exec('ant retrieveUnpackaged', function(err, result) {
+                    if(err) return console.log(err);
+                    // res.send('package.xml successfully saved!\n' + result);
+                    console.log('[ Salesforce package successfully retrieved ]');
+                    console.log(result);
+                });
+
+            });
+        });
+    });
 });
